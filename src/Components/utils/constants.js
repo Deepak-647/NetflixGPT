dbd3fb7af8934db28b4e671a608e4a1607549449
unknown <dbehera647@gmail.com>
2024-11-02 17:52:49 +0530

    getting the movies data from TMDB API and putting it to redux store

--- src/Components/utils/constants.js
+++ src/Components/utils/constants.js
@@ -1,3 +1,14 @@
-export const PHOTO_URL = "https://avatars.githubusercontent.com/u/82494501?v=4&size=64";
-export const BODY_IMG = "https://assets.nflxext.com/ffe/siteui/vlv3/7c0e18aa-2c95-474d-802e-7f30e75dcca4/web/IN-en-20241014-TRIFECTA-perspective_e7121311-c11e-4809-a3e6-22abffa33569_large.jpg";
-export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+export const PHOTO_URL =
+  "https://avatars.githubusercontent.com/u/82494501?v=4&size=64";
+export const BODY_IMG =
+  "https://assets.nflxext.com/ffe/siteui/vlv3/7c0e18aa-2c95-474d-802e-7f30e75dcca4/web/IN-en-20241014-TRIFECTA-perspective_e7121311-c11e-4809-a3e6-22abffa33569_large.jpg";
+export const LOGO =
+  "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNjk4MTI4MjczZTlhZTI3ODQ1YjI2NTI4NjM5NzcwZSIsIm5iZiI6MTczMDUyNjM4Ni42NzUxNjk3LCJzdWIiOiI2M2ZiMDE2NDZhYThlMDAwZjBiNmJmZTEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.fzrRBofBiKM3fK5UPaVy__Uow9SK_1Eg9ztOLkOac1I",
+  },
+};
