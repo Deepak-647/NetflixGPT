796e897b391d74278683da984c1da5ef6c45eb86
unknown <dbehera647@gmail.com>
2024-11-03 01:34:30 +0530

    Created custom hook for trailer video

--- src/Components/VideoContainer.js
+++ src/Components/VideoContainer.js
@@ -1,31 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { API_OPTIONS } from "./utils/constants";
-import { useDispatch, useSelector } from "react-redux";
-import { addMovieTrailer } from "./utils/moviesSlice";
+import React from "react";
+
+import { useSelector } from "react-redux";
+import useMovieTrailer from "./hooks/useMovieTrailer";
 
 const VideoContainer = ({ movieId }) => {
-    const trailerId = useSelector(store => store.movies?.movieTrailer)
-    const dispatch = useDispatch();
-
-    // const [trailerId,setTrailerId]= useState(null);
-
-  const getMovieVideo = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/912649/videos",
-      API_OPTIONS
-    );
-    const json = await data.json();
-    
-
-    const filterData = json.results.filter((video) => video.type === "Trailer");
-    const trailer = filterData.length ? filterData[0] : json.results[0];
-    console.log(trailer);
-    // setTrailerId(trailer.key)
-    dispatch(addMovieTrailer(trailer))
-  };
-  useEffect(() => {
-    getMovieVideo();
-  }, []);
+  const trailerId = useSelector((store) => store.movies?.movieTrailer);
+ useMovieTrailer(movieId)
   return (
     <div>
       <iframe
@@ -33,10 +13,8 @@ const VideoContainer = ({ movieId }) => {
         height="315"
         src={"https://www.youtube.com/embed/" + trailerId?.key}
         title="YouTube video player"
-      
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerpolicy="strict-origin-when-cross-origin"
-       
       ></iframe>
     </div>
   );
