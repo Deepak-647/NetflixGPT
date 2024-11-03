36807c64bf157a05fc5174f3a400f3baffebc102
unknown <dbehera647@gmail.com>
2024-11-03 12:00:12 +0530

    Update the store with trailer data and embedded the YouTube video

--- src/Components/VideoContainer.js
+++ src/Components/VideoContainer.js
@@ -5,13 +5,16 @@ import useMovieTrailer from "./hooks/useMovieTrailer";
 
 const VideoContainer = ({ movieId }) => {
   const trailerId = useSelector((store) => store.movies?.movieTrailer);
- useMovieTrailer(movieId)
+  useMovieTrailer(movieId);
   return (
     <div>
       <iframe
-        width="560"
-        height="315"
-        src={"https://www.youtube.com/embed/" + trailerId?.key}
+        className="w-screen aspect-video"
+        src={
+          "https://www.youtube.com/embed/" +
+          trailerId?.key +
+          "?&autoplay=1&mute=1"
+        }
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerpolicy="strict-origin-when-cross-origin"
--- src/Components/VideoTitle.js
+++ src/Components/VideoTitle.js
@@ -1,20 +1,19 @@
 import React from "react";
-import { FaPlay,FaInfoCircle } from "react-icons/fa";
+import { FaPlay, FaInfoCircle } from "react-icons/fa";
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="pt-36 px-12">
+    <div className=" w-screen aspect-video pt-[15%] px-24 absolute text-white bg-gradient-to-r from-black  ">
       <h1 className="text-5xl font-bold">{title}</h1>
       <p className="text-lg w-1/3 py-8">{overview}</p>
       <div className="flex">
-        <button className="bg-gray-700 text-white p-2 px-8 rounded-lg mx-2 flex items-center">
-          <FaPlay className="text-white mx-2" />
+        <button className=" text-black bg-white p-2 px-8 rounded-lg mx-2 flex items-center hover:bg-opacity-80">
+          <FaPlay className=" text-black mx-2" />
           Play
         </button>
-        <button className="bg-gray-700 text-white p-2 px-8 rounded-lg mx-2 flex items-center">
-        <FaInfoCircle className="text-white mx-2"/>
+        <button className="bg-slate-800 text-white p-2 px-8 rounded-lg mx-2 flex items-center">
+          <FaInfoCircle className="text-white mx-2" />
           More info
-          
         </button>
       </div>
     </div>
